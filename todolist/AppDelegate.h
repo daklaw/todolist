04f078bffc7ef55b381d6820252471d9619485f2
David Law <davidlaw@61.87.208.web-pass.com>
2014-01-26 13:35:50 -0800

    Initial Commit

---
+++ todolist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todolist
+//
+//  Created by David Law on 1/26/14.
+//  Copyright (c) 2014 David Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
